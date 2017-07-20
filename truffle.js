dee0270f3388c234622fa67650fe1fe963d3dbc5
Leonid Beder <leonid.beder@gmail.com>
2017-07-19 20:14:39 -0400

    Added truffle.js.

---
+++ truffle.js
@@ -0,0 +1,27 @@
+require('babel-register');
+require('babel-polyfill');
+
+let provider;
+
+module.exports = {
+    networks: {
+        development: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*' // Match any network id
+        },
+        docker: {
+            host: 'testrpc',
+            port: 8545,
+            network_id: '*'
+        },
+        ropsten: {
+            provider: provider,
+            network_id: 3 // official id of the ropsten network
+        }
+    },
+    mocha: {
+        useColors: true,
+        slow: 30000
+    }
+};
