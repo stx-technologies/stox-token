48cb9b59d791b2f4e39644cf2f613eab8293d4b5
Leonid Beder <leonid.beder@gmail.com>
2017-07-20 17:49:09 -0400

    Added mine helper.

---
+++ test/helpers/time.js
@@ -0,0 +1,18 @@
+const mine = () => {
+    return new Promise((resolve, reject) => {
+        web3.currentProvider.sendAsync({
+            jsonrpc: '2.0',
+            method: 'evm_mine',
+            params: [],
+            id: new Date().getTime()
+        }, (err) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
+    });
+}
+
+export default { mine };
