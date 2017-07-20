a5d510251f0dd56802aa4847fea799b496986644
Leonid Beder <leonid.beder@gmail.com>
2017-07-19 20:16:45 -0400

    Added Ownable and SafeMath.

---
+++ contracts/Ownable.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.11;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control functions, this simplifies
+ & the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+    address public newOwnerCandidate;
+
+    event OwnershipRequested(address indexed _by, address indexed _to);
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            throw;
+        }
+
+        _;
+    }
+
+    /**
+     * @dev Proposes to transfer control of the contract to a newOwnerCandidate.
+     * @param _newOwnerCandidate address The address to transfer ownership to.
+     */
+    function requestOwnershipTransfer(address _newOwnerCandidate) onlyOwner {
+        require(_newOwnerCandidate != address(0));
+
+        newOwnerCandidate = _newOwnerCandidate;
+
+        OwnershipRequested(msg.sender, newOwnerCandidate);
+    }
+
+    /**
+     * @dev Accept ownership transfer. This method needs to be called by the perviously proposed owner.
+     */
+    function acceptOwnership() {
+        if (msg.sender == newOwnerCandidate) {
+            owner = newOwnerCandidate;
+            newOwnerCandidate = address(0);
+
+            OwnershipTransferred(owner, newOwnerCandidate);
+        }
+    }
+}
---
+++ test/Ownable.js
@@ -0,0 +1,68 @@
+import expectThrow from './helpers/expectThrow';
+const Ownable = artifacts.require('../contracts/Ownable.sol');
+
+contract('Ownable', (accounts) => {
+    let ownable;
+
+    let owner = accounts[0];
+    let newOwner = accounts[1];
+    let stranger = accounts[2];
+
+    beforeEach(async () => {
+        ownable = await Ownable.new();
+    });
+
+    describe('construction', async () => {
+        it('should have an owner', async () => {
+            assert.equal(await ownable.owner(), owner);
+        });
+
+        it('should not have a newOwnerCandidate', async () => {
+            assert.equal(await ownable.newOwnerCandidate(), 0);
+        });
+    });
+
+    describe('ownership transfer', async () => {
+        it('should change newOwnerCandidate', async () => {
+            await ownable.requestOwnershipTransfer(newOwner);
+
+            assert.equal(await ownable.newOwnerCandidate(), newOwner);
+        });
+
+        it('should not change owner without approving the new owner', async () => {
+            await ownable.requestOwnershipTransfer(newOwner);
+
+            assert.equal(await ownable.owner(), owner);
+        });
+
+        it('should change owner after transfer and approval', async () => {
+            await ownable.requestOwnershipTransfer(newOwner);
+            await ownable.acceptOwnership({from: newOwner});
+
+            assert.equal(await ownable.owner(), newOwner);
+            assert.equal(await ownable.newOwnerCandidate(), 0);
+        });
+
+        it('should prevent non-owners from transfering ownership', async () => {
+            assert((await ownable.owner()) != stranger);
+
+            await expectThrow(ownable.requestOwnershipTransfer(newOwner, {from: stranger}));
+        });
+
+        it('should prevent transferring ownership to null or 0 address', async () => {
+            await expectThrow(ownable.requestOwnershipTransfer(null, {from: owner}));
+            await expectThrow(ownable.requestOwnershipTransfer(0, {from: owner}));
+
+            assert.equal(owner, await ownable.owner());
+        });
+
+        it('should prevent strangers from accepting ownership', async () => {
+            await ownable.requestOwnershipTransfer(newOwner);
+            assert.equal(await ownable.newOwnerCandidate(), newOwner);
+
+            await ownable.acceptOwnership({from: stranger});
+            assert.equal(await ownable.newOwnerCandidate(), newOwner);
+            assert.equal(await ownable.owner(), owner);
+        });
+    });
+});
