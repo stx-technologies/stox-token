0a06dc8de070369ebce06bbb984082f8e8df89b4
Leonid Beder <leonid.beder@gmail.com>
2017-07-29 22:25:10 +0300

    Removed "token" from the name of the token.

--- contracts/StoxSmartToken.sol
+++ contracts/StoxSmartToken.sol
@@ -8,6 +8,6 @@ import './SaferMath.sol';
 contract StoxSmartToken is SmartToken {
     using SaferMath for uint256;
 
-    function StoxSmartToken() SmartToken('Stox Token', 'STX', 18) {
+    function StoxSmartToken() SmartToken('Stox', 'STX', 18) {
     }
 }
