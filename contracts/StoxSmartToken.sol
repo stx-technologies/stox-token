4b953a8bf07d129037c2bc71525ac52e608bb36d
Leonid Beder <leonid.beder@gmail.com>
2017-07-31 00:02:45 +0300

    Removed unneeded library.

--- contracts/StoxSmartToken.sol
+++ contracts/StoxSmartToken.sol
@@ -2,12 +2,8 @@ pragma solidity ^0.4.11;
 
 import 'bancor-contracts/solidity/contracts/SmartToken.sol';
 
-import './SaferMath.sol';
-
 /// @title Stox Smart Token
 contract StoxSmartToken is SmartToken {
-    using SaferMath for uint256;
-
     function StoxSmartToken() SmartToken('Stox', 'STX', 18) {
     }
 }
